66367d106b150ec7439e631002a08863832c53d9
乔达摩悉达多 <xifanboss@users.noreply.github.com>
2017-05-22 12:09:12 +0800

    Add files via upload

--- 01第一个Python猜数小游戏.py
+++ 01第一个Python猜数小游戏.py
@@ -8,17 +8,12 @@
 # 非淡泊无以明志，非宁静无以致远
 # 长风破浪会有时，直挂云帆济沧海
 
-print ('====猜猜我是谁？=====')
+print('====猜猜我是谁？=====')
 temp = input('不妨猜一下我猜的数字：')
 guess = int(temp)
-if guess ==8:
-    print ('哈哈，你是蛔虫吗？')
-    print ('猜对了也没有奖励！')
+if guess == 8:
+    print('哈哈，你是蛔虫吗？')
+    print('猜对了也没有奖励！')
 else:
-    print ('骚瑞！你没有猜对哦！')
-print ('Game Over!')
-
-
-
-
-
+    print('骚瑞！你没有猜对哦！')
+print('Game Over!')
