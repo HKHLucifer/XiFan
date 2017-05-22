203f2a22bdec56e1be2a2437519edcd2037c3a27
乔达摩悉达多 <xifanboss@users.noreply.github.com>
2017-05-22 10:57:15 +0800

    Add files via upload

---
+++ 01第一个Python猜数小游戏.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Time    : 2017/5/18 15:41
+# @Author  : XiFan
+# @Web     : VPSZN.NET
+
+# 非淡泊无以明志，非宁静无以致远
+# 长风破浪会有时，直挂云帆济沧海
+
+print ('====猜猜我是谁？=====')
+temp = input('不妨猜一下我猜的数字：')
+guess = int(temp)
+if guess ==8:
+    print ('哈哈，你是蛔虫吗？')
+    print ('猜对了也没有奖励！')
+else:
+    print ('骚瑞！你没有猜对哦！')
+print ('Game Over!')
+
+
+
+
+
---
+++ 02变量名和字符串.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Time    : 2017/5/18 15:41
+# @Author  : XiFan
+# @Web     : VPSZN.NET
+
+# 非淡泊无以明志，非宁静无以致远
+# 长风破浪会有时，直挂云帆济沧海
+
+wb = '王八蛋'
+print (wb)
+wb = '老乌龟'
+print (wb)
+
+a=3
+b=8
+c=a+b
+print (c)
+
+my ='什么鬼'
+you= '黑夜啊'
+she=my+you
+print (she)
+
+cs = r"C:\Program Files\alipay"       #r为原始字符串
+print (cs)
+
+cd ="""
+纤云弄巧，飞星传恨，银汉迢迢暗度。
+金风玉露一相逢，便胜却人间无数。
+柔情似水，佳期如梦，忍顾鹊桥归路。
+两情若是久长时，又岂在朝朝暮暮。
+"""
+print (cd)
